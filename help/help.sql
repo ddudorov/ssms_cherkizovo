be0276ccfdc46809eed7a61d71fe3349c03d3c09
Mikhail <xayc@mail.ru>
2019-11-26 12:05:37 +0300

    Test 2
    Co-Authored-By: Mikhail <focha13@users.noreply.github.com>

--- help/help.sql
+++ help/help.sql
@@ -1,1 +1,95 @@
-use cherkizovo; -- подключение к Ѕƒ
+
+select FORMAT(123456666789,'0-00-0')
+select convert(dec(4,3),0.02)
+select ISNUMERIC('134')
+select ORIGINAL_LOGIN()					
+
+select FLOOR(1.256)						-- округление вниз / отбрасывает дробную часть
+select CEILING(1.256)					-- округление вверх
+select UPPER('ЅоЋьЎая Ѕу вј')			-- верхний регистр
+select LOWER('ћаЋеЌь ая Ѕу вј')			-- нижний регистр
+
+
+
+select @@version						-- верси€ сервера Microsoft SQL Server 2017 (RTM-CU13-OD) (KB4483666) - 14.0.3049.1 (X64)  on Windows Server 2016 Standard 10.0 <X64> (Build 14393: ) (Hypervisor) 
+use cherkizovo;							-- подключение к Ѕƒ
+kill 800								-- завершить є сессию
+select ROUND(15.123456789, 0, 1)        -- округление
+
+
+
+--------------------
+-- –јЅќ“ј — “јЅЋ»÷≈…
+--------------------
+begin 
+
+			
+			-- проверка и удаление таблицы
+			IF OBJECT_ID('tempdb..#table','U')	is not null drop table #table;
+			IF OBJECT_ID('db.dbo.table','U')	is not null drop table db.dbo.[table];
+
+			-- создание таблицы
+			create table #test	
+			(	
+				 id								int					not	null	IDENTITY(1,1)		
+				,id_text						varchar(100)		not	null
+				,dt_tm							datetime			not null 	default getdate()
+				,year_from_dt_tm												as datepart(year,dt_tm) 
+				 PRIMARY KEY (id)
+				,UNIQUE (id_text) 
+			);
+			
+			TRUNCATE TABLE #table											-- очистить таблицу
+
+			ALTER TABLE #test ADD			column_name smallint not null;	-- добавление стобца
+			ALTER TABLE #test DROP COLUMN	column_name;					-- удаление стобца
+
+			ALTER INDEX ALL ON #table DISABLE;								-- отключить все индексы
+			ALTER INDEX ALL ON #table REBUILD;								-- включить  все индексы
+
+			DISABLE TRIGGER Products_INSERT_UPDATE ON Products				
+			ENABLE TRIGGER Products_INSERT_UPDATE ON Products				
+
+
+
+end;
+
+
+
+
+-----------------------
+-- –јЅќ“ј — ѕ≈–≈ћ≈ЌЌЌќ…
+-----------------------
+begin 
+
+			declare @v varchar(10)					-- объ€вление переменных
+				set @v='Ћеша'						-- присвоение значений
+			print @v								-- результат
+			select @v='Ћеша1'						-- присвоение значений в запросе
+			select @v								-- результат
+
+end;
+
+
+
+-------------------------
+-- ќЅ–јЅќ“ ј ќЎ»Ѕќ  ¬ SQL
+-------------------------
+begin 
+
+			-- обработка ошибок в SQL
+			BEGIN TRY  
+						SELECT 1/1;  
+						SELECT 1/0;  
+			END TRY  
+			BEGIN CATCH  
+						SELECT  
+							 ERROR_NUMBER()		AS ErrorNumber  
+							,ERROR_SEVERITY()	AS ErrorSeverity  
+							,ERROR_STATE()		AS ErrorState  
+							,ERROR_PROCEDURE()	AS ErrorProcedure  
+							,ERROR_MESSAGE()	AS ErrorMessage;  
+			END CATCH;  
+		
+
+end;
